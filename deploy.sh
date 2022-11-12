bac76cbdc8f4a9eaae7925987917441d442af221
unknown <adonamarkkevin@gmail.com>
2022-11-12 13:52:30 +0800

    fixed typo

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@ docker rm gentri_be_queue_container
 docker rmi gentri_be_queue_img
 
 docker build -t gentri_be_queue_img .
-docker run -d -p 9000:9000 --name gentri_be_queue_img_queue_container gentri_be_queue_img
+docker run -d -p 9000:9000 --name gentri_be_queue_container gentri_be_queue_img
 docker image prune -f
